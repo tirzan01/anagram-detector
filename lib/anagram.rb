92bc22221c60f5707b431009da936ac160309cc8
thomas <thomasmonta98@gmail.com>
2021-08-30 13:02:44 +1000

    done with assignment

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,18 @@
 # Your code goes here!
+class Anagram
+
+  attr_accessor  :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(strings)
+    matches = []
+    strings.each do |string|
+      matches << string if string.split('').sort == @word.split('').sort
+    end
+    matches
+  end
+
+end
